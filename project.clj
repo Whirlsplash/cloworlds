ae79053e9ea04d77cff931dc0634e73bc22200f6
Fuwn <fuwnzy@gmail.com>
2021-04-20 18:40:23 -0700

    chore: Update version ID

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.fun/worlds "1.0.0"
+(defproject org.clojars.fun/worlds "1.0.1"
   :description "A Clojure library for interacting with Worlds."
   :url "https://github.com/Whirlsplash/cloworlds"
-  :license {:name "GPLv3"
+  :license {:name "GPL-3.0-only"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [http.async.client "1.3.1"]]
